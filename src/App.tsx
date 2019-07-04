b33b38282169dbac18b06be0cc871994778c8dc9
chanLettuce <lettuceandlettuce@gmail.com>
2019-07-05 07:27:26 +0900

    いろいろ整理がついた

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Header from './components//Header/Header';
 import Root from './components/Root/Root';
 import Profile from './components/Profile/Profile';
 
 const App = () => (
-  <div id="react-app">
-    <Header title="ちゃんレタス" />
-    <div id="contents">
-      <Switch>
-        <Route exact path="/" component={Root} />
-        <Route exact path="/profile" component={Profile} />
-      </Switch>
+  <HashRouter>
+    <div id="react-app">
+      <Header title="ちゃんレタス" />
+      <div id="contents">
+        <Switch>
+          <Route exact path="/" component={Root} />
+          <Route exact path="/profile" component={Profile} />
+        </Switch>
+      </div>
     </div>
-  </div>
+  </HashRouter>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import { BrowserRouter, Route } from 'react-router-dom';
 import ReactGA from 'react-ga';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -12,12 +11,7 @@ ReactGA.initialize('UA-120838241-3');
 ReactGA.set({ page: pathname });
 ReactGA.pageview(pathname);
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Route path="/" component={App} />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
