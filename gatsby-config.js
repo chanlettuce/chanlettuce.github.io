5570903432111d940b153ef0cc282ab0086bd296
chanlettuce <37320700+chanlettuce@users.noreply.github.com>
2021-05-16 20:44:38 +0900

    メタデータの記述

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `LettuceWork / Sho Uchida`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Sho Uchida`,
+      summary: `Web Application Developer`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `LettuceWork(内田 翔)のポートフォリオです`,
+    siteUrl: `https://aqours.tech/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `chanLettuce01`,
     },
   },
   plugins: [
