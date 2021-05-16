2960141a8e3b554cb2acb3b71dca28969abc5d27
chanlettuce <37320700+chanlettuce@users.noreply.github.com>
2021-05-16 20:46:26 +0900

    プロフィール概要追加

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,88 @@
+import { graphql, Link, PageProps } from "gatsby";
+import React from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import Layout from "../../components/layout";
+import { myHistory } from "../../constants/myHistory";
+
+const ProfileIndex: React.FC<PageProps<GatsbyTypes.ProfileIndexQuery>> = ({
+  data,
+  location,
+}) => {
+  const siteTitle = data.site?.siteMetadata?.title || `Title`;
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Row>
+        <Col as="article" lg={6}>
+          <h2>基本情報</h2>
+          <section>
+            <dl>
+              <div>
+                <dt>屋号</dt>
+                <dd>LettuceWork</dd>
+              </div>
+              <div>
+                <dt>名前</dt>
+                <dd>内田 翔 (うちだ しょう)</dd>
+              </div>
+              <div>
+                <dt>生年月日</dt>
+                <dd>1994年2月8日</dd>
+              </div>
+              <div>
+                <dt>住所</dt>
+                <dd>東京都 千代田区</dd>
+              </div>
+              <div>
+                <dt>対応可能業務</dt>
+                <dd>
+                  <ul>
+                    <li>
+                      Webアプリケーション開発(クラウド, オンプレどちらでも可能)
+                    </li>
+                    <li>
+                      小規模なオフィスインフラの構築(ネットワーク,
+                      ファイルサーバー等)
+                    </li>
+                    <li>その他コンピューター関連の問題解決</li>
+                  </ul>
+                </dd>
+                <Button variant="outline-dark" as={Link} to="/">
+                  詳細はこちら
+                </Button>
+              </div>
+            </dl>
+          </section>
+        </Col>
+        <Col as="article" lg={6}>
+          <h2>略歴</h2>
+          <section>
+            {myHistory
+              .filter(e => e.business)
+              .map((e, i) => (
+                <div key={`history-${i}`}>
+                  <dt>{e.date}</dt>
+                  <dd>{e.description}</dd>
+                </div>
+              ))}
+            <Button variant="outline-dark" as={Link} to="/">
+              詳細はこちら
+            </Button>
+          </section>
+        </Col>
+      </Row>
+    </Layout>
+  );
+};
+
+export default ProfileIndex;
+
+export const pageQuery = graphql`
+  query ProfileIndex {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
